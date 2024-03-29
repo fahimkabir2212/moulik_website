62c15591e0101d73d51a3c4b817c87ebbb8cf036
Fahim Kabir <fahimkabir2213@gmail.com>
2024-03-29 10:48:51 +0600

    Added hero slider along with all the images

---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // Initialize Swiper
+  var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+      delay: 5000, // 5 seconds delay between slides
+    },
+  });
+});
